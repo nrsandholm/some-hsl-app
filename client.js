78b7defd7f8fb5c995b1f2eb9a496070b38bd75f
Niko <me@home>
2021-02-19 02:52:10 +0200

    Query stops and routes from HSL API

---
+++ client.js
@@ -0,0 +1,40 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  gql,
+} from '@apollo/client';
+
+export const client = new ApolloClient({
+  uri: 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql',
+  cache: new InMemoryCache()
+});
+
+export const getStopsByLocationQuery = (lat, lon, radius) => {
+  return gql`{
+	  stopsByRadius(lat:${lat}, lon:${lon}, radius:${radius}) {
+	    edges {
+	      node {
+	        stop {
+	          gtfsId
+	          name
+	        }
+	        distance
+	      }
+	    }
+	  }
+	}`;
+}
+
+export const getStopByIdQuery = (id) => {
+	return gql`{
+    stop (id: "${id}") {
+      gtfsId
+      name
+      routes {
+        gtfsId
+        shortName
+        longName
+      }
+    }
+	}`;
+}
--- index.js
+++ index.js
@@ -9,63 +9,98 @@ import {
   Redirect,
 } from 'react-router-dom';
 import './index.css';
-import { getStops } from './service';
-
-const stops = getStops();
+import {
+  client,
+  getStopsByLocationQuery,
+  getStopByIdQuery,
+} from './client';
+import {
+  ApolloProvider,
+  useQuery
+} from '@apollo/client';
 
 const App = () => (
-	<BrowserRouter>
-    <div className="App">
-      <Switch>
-        <BrowserRoute path="/stops/:stopId/routes">
-          <Routes />
-        </BrowserRoute>
-        <BrowserRoute path="/stops">
-          <Stops />
-        </BrowserRoute>
-        <BrowserRoute path="*">
-          <Redirect to="/stops"/>;
-        </BrowserRoute>
-      </Switch>
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <BrowserRoute path="/stops/:stopId/routes">
+            <Routes />
+          </BrowserRoute>
+          <BrowserRoute path="/stops">
+            <Stops />
+          </BrowserRoute>
+          <BrowserRoute path="*">
+            <Redirect to="/stops"/>;
+          </BrowserRoute>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </ApolloProvider>
+);
+
+const Stops = () => {
+  const { loading, error, data } = useQuery(getStopsByLocationQuery(60.18693365313273, 24.95368673076361, 100));
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error!</div>;
+  }
+
+  const nodes = data
+    .stopsByRadius
+    .edges
+    .reduce((nodes, edge) => {
+      const { node } = edge;
+      return nodes.concat(node);
+    }, []);
+
+	return (
+    <div className="Stops">
+      {nodes.map((node) => {
+        return <Stop key={node.stop.gtfsId} stop={node.stop} distance={node.distance} />
+      })}
     </div>
-  </BrowserRouter>
-);
+  );
+};
 
-const Stops = () => (
-	<div className="Stops">
-		{stops.map((stop) => {
-      return <Stop key={stop.id} stop={stop} />
-    })}
-	</div>
-);
-
-const Stop = ({ stop }) => (
+const Stop = ({ stop, distance }) => (
 	<div className="Stop">
-		<Link to={`/stops/${stop.id}/routes`}>{stop.name}</Link>
+		<Link to={`/stops/${stop.gtfsId}/routes`}>{stop.name}</Link>
+    <div>{distance}m</div>
 	</div>
 );
 
 const Routes = () => {
   const { stopId } = useParams();
-  const stopIdInt = parseInt(stopId);
-  const { routes } = stops.find(stop => stop.id === stopIdInt);
+
+  const { loading, error, data } = useQuery(getStopByIdQuery(stopId));
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error!</div>;
+  }
+
+  const { routes } = data.stop;
 
 	return (
     <div className="Routes">
       <div>
         <Link to={`/`}>Stops</Link>
       </div>
-      {routes.map((route) => {
-          return <Route key={route.id} route={route} />
-      })}
+      {routes.map((route) => <Route key={route.gtfsId} route={route} />)}
     </div>
   )
 }
 
 const Route = ({ route }) => (
   <div className="Route">
-    <div>{route.id}</div>
-    <div>{route.name}</div>
+    <div>{route.shortName}</div>
+    <div>{route.longName}</div>
   </div>
 )
 
