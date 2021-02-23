607b82c8f0aaef62f3fd2f95a53ef9657f709284
Niko <me@home>
2021-02-23 10:05:43 +0200

    Setup domain and HTTPS support for S3 bucket

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -3,5 +3,17 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { InfraStack } from '../lib/infra-stack';
 
+const stackProps = {
+  env: {
+    account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION
+  }
+};
+
+const siteProps = {
+  domainName: 'some-hsl-app.tk'
+}
+
 const app = new cdk.App();
-new InfraStack(app, 'InfraStack');
+
+new InfraStack(app, 'InfraStack', siteProps, stackProps);
--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,14 +1,55 @@
-import * as cdk from '@aws-cdk/core';
+import { DnsValidatedCertificate } from '@aws-cdk/aws-certificatemanager';
+import { CloudFrontWebDistribution, OriginProtocolPolicy, SSLMethod, SecurityPolicyProtocol } from '@aws-cdk/aws-cloudfront';
+import { ARecord, RecordTarget, HostedZone } from '@aws-cdk/aws-route53';
+import { CloudFrontTarget } from '@aws-cdk/aws-route53-targets/lib';
 import { Bucket } from '@aws-cdk/aws-s3';
 import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment';
+import * as cdk from '@aws-cdk/core';
+
+export interface SiteProps {
+  domainName: string;
+  subDomainName?: string;
+}
 
 export class InfraStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+  constructor(scope: cdk.Construct, id: string, props: SiteProps, stackProps: cdk.StackProps) {
+    super(scope, id, stackProps);
+
+    const zone = HostedZone.fromLookup(this, 'Zone', { domainName: props.domainName });
+    const siteDomain = [props.subDomainName, props.domainName].filter(Boolean).join('.');
+    new cdk.CfnOutput(this, 'Site', { value: 'https://' + siteDomain });
 
     const websiteBucket = new Bucket(this, 'WebsiteBucket', {
       websiteIndexDocument: 'index.html',
       publicReadAccess: true
+    });
+
+    const certificateArn = new DnsValidatedCertificate(this, 'SiteCertificate', {
+      domainName: siteDomain,
+      hostedZone: zone,
+      region: 'eu-central-1',
+    }).certificateArn;
+
+    const distribution = new CloudFrontWebDistribution(this, 'SiteDistribution', {
+      aliasConfiguration: {
+        acmCertRef: certificateArn,
+        names: [ siteDomain ],
+        sslMethod: SSLMethod.SNI,
+        securityPolicy: SecurityPolicyProtocol.TLS_V1_1_2016,
+      },
+      originConfigs: [{
+        customOriginSource: {
+            domainName: websiteBucket.bucketWebsiteDomainName,
+            originProtocolPolicy: OriginProtocolPolicy.HTTP_ONLY,
+        },
+        behaviors : [ {isDefaultBehavior: true}],
+      }]
+    });
+
+    new ARecord(this, 'SiteAliasRecord', {
+      recordName: siteDomain,
+      target: RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
+      zone
     });
     
     new BucketDeployment(this, 'DeployWebsite', {
